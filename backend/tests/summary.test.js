0a1b72499f19d77529442cb61ba1e85035305549
JoseContreras <jocontrera16@gmail.com>
2020-10-18 23:16:45 -0300

    test for summaries is ready

---
+++ backend/tests/summary.test.js
@@ -0,0 +1,12 @@
+import server from '../src/index';
+import request from 'supertest';
+import {expect} from 'chai';
+
+
+describe('Summaries API Tests', () => {
+    it('GET /api/v1/summaries/ returns an array of Summaries', async () => {
+        const response = await request(server).get('/api/v1/summaries/');
+        expect(response.status).to.equal(200);
+        expect(response.body).to.be.an.instanceOf(Array);
+    });
+});
