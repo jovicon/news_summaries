9e0463f9b395d11f3b5799ef7fcc69be88800819
JoseContreras <jocontrera16@gmail.com>
2020-10-25 22:16:55 -0300

    decopling db connection

---
+++ backend/db/dbSettings.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import config from 'config';
+
+export const mongoConfig = () => {
+    mongoose.connect(config.get('database'), {
+       useNewUrlParser: true
+   });
+   
+   mongoose.connection.once("open", () => {
+       console.log("MongoDB database connection established successfully.")
+   });
+}
