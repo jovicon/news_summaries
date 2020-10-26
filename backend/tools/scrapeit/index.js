90847825f92eaafe4d64ef4a1b493b2bc0671c6a
JoseContreras <jocontrera16@gmail.com>
2020-10-25 22:19:48 -0300

    adding scrapper function and type module on package.json

--- backend/tools/scrapeit/index.js
+++ backend/tools/scrapeit/index.js
@@ -1,36 +1,23 @@
-// bbc.com, cnnespanol.com, vice.com
-// vice.com/es
+// CommonJs
+// const config = require('config');
+// const mongoose = require('mongoose');
+// const scrapeIt = require("scrape-it");
+// const Summary = require('../../src/models/Summary')
 
-const config = require('config');
-const mongoose = require('mongoose');
-const scrapeIt = require("scrape-it");
-const Summary = require('../../src/models/Summary')
+// ES6
+import config from 'config';
+import { scrapeItAsync as scraping } from '../loaderES6/index.js';
+import { Summary } from '../../src/models/Summary.js';
+import { mongoConfig } from '../../db/dbSettings.js';
 
-mongoose.connect(config.get('database'), {
-    useNewUrlParser: true
-});
+mongoConfig();
 
-mongoose.connection.once("open", () => {
-    console.log("MongoDB database connection established successfully.")
-});
-
-const scraping = async function(url, scrapingStructure) {
-    const news = await scrapeIt(url, scrapingStructure)
-    .then(({data, response}) => {
-        console.log(`Status Code: ${response.statusCode}`);
-        return data;
-    })
-    .catch( error => {
-        console.log(error);
-    });
-    return news;
-}
-
-const save_scraping = async (url, googleStructure) => {
+export let saveScraping = async (url, googleStructure) => {
     const news = await scraping(url,googleStructure);
 
     news.articles.map((summary) => {
-        summarySave = new Summary(summary);
+
+        let summarySave = new Summary(summary);
 
         summarySave.save()
         .then((data) => {
@@ -40,159 +27,4 @@ const save_scraping = async (url, googleStructure) => {
             console.log(error);
         })
     });
-}
-
-// const googleUrl = "https://news.google.com/topstories";
-const googleUrl = "https://news.google.cl/topstories";
-const googleStructure = {
-    articles: {
-        listItem: ".NiLAwe",
-        data: {
-            title: "h3.ipQwMb.ekueJc.RD0gLb",
-            url: {
-                selector: "h3.ipQwMb.ekueJc.RD0gLb a",
-                attr: "href"
-            }
-        }
-    }
-}
-save_scraping(googleUrl, googleStructure);
-
-
-// const bbcUrl = "https://www.bbc.com/news";
-// const bbcStructureA = {
-//     articles: {
-//         listItem: ".gel-layout__item.nw-c-top-stories__primary-item",
-//         data: {
-//             title: "h3.gs-c-promo-heading__title.gel-paragon-bold.nw-o-link-split__text",
-//             url: {
-//                 selector: "a.gs-c-promo-heading.gel-paragon-bold",
-//                 attr: "href"
-//             }
-//         }
-//     }
-// }
-// const bbcStructureB = {
-//     articles: {
-//         listItem: ".gel-layout__item.nw-c-top-stories__secondary-item",
-//         data: {
-//             title: "h3.gs-c-promo-heading__title.gel-pica-bold.nw-o-link-split__text",
-//             url: {
-//                 selector: "a.gs-c-promo-heading.gs-o-faux-block-link__overlay-link.gel-pica-bold.nw-o-link-split__anchor",
-//                 attr: "href"
-//             }
-//         }
-//     }
-// }
-// const bbcStructureC = {
-//     articles: {
-//         listItem: ".gel-layout__item.nw-c-top-stories__tertiary-items",
-//         data: {
-//             title: "h3.gs-c-promo-heading__title.gel-pica-bold.nw-o-link-split__text",
-//             url: {
-//                 selector: "a.gs-c-promo-heading.gs-o-faux-block-link__overlay-link.gel-pica-bold.nw-o-link-split__anchor",
-//                 attr: "href"
-//             }
-//         }
-//     }
-// }
-
-// const bbcStructureD = {
-//     articles: {
-//         listItem: "#u6098469211298796 .gel-layout__item.gs-o-faux-block-link",
-//         data: {
-//             title: "span.gs-c-promo-heading__title.gel-pica-bold",
-//             url: {
-//                 selector: "a.gs-c-promo-heading.nw-o-link.gs-o-bullet__text",
-//                 attr: "href"
-//             }
-//         }
-//     }
-// }
-
-
-
-// save_scraping(bbcUrl, bbcStructureA);
-// save_scraping(bbcUrl, bbcStructureB);
-// save_scraping(bbcUrl, bbcStructureC);
-// save_scraping(bbcUrl, bbcStructureD);
-
-
-
-// const google_news = scraping("https://news.google.com/topstories")
-//                         .then(response => { console.log(response)});
-
-// console.log(google_news);
-
-// Promise interface
-// const scrape = scrapeIt("https://ionicabizau.net", {
-//     title: ".header h1"
-//   , desc: ".header h2"
-//   , avatar: {
-//         selector: ".header img"
-//       , attr: "src"
-//     }
-// }).then(({ data, response }) => {
-//     console.log(`Status Code: ${response.statusCode}`)
-//     console.log(data)
-// })
-
-
-// Callback interface
-// scrapeIt("https://ionicabizau.net", {
-//     // Fetch the articles
-//     articles: {
-//         listItem: ".article"
-//       , data: {
-
-//             // Get the article date and convert it into a Date object
-//             createdAt: {
-//                 selector: ".date"
-//               , convert: x => new Date(x)
-//             }
-
-//             // Get the title
-//           , title: "a.article-title"
-
-//         //     // Nested list
-//         //   , tags: {
-//         //         listItem: ".tags > span"
-//         //     }
-
-//         //     // Get the content
-//         //   , content: {
-//         //         selector: ".article-content"
-//         //       , how: "html"
-//         //     }
-
-//         //     // Get attribute value of root listItem by omitting the selector
-//         //   , classes: {
-//         //         attr: "class"
-//         //     }
-//         }
-//     }
-
-// //     // Fetch the blog pages
-// //   , pages: {
-// //         listItem: "li.page"
-// //       , name: "pages"
-// //       , data: {
-// //             title: "a"
-// //           , url: {
-// //                 selector: "a"
-// //               , attr: "href"
-// //             }
-// //         }
-// //     }
-
-// //     // Fetch some other data from the page
-// //   , title: ".header h1"
-// //   , desc: ".header h2"
-// //   , avatar: {
-// //         selector: ".header img"
-// //       , attr: "src"
-// //     }
-
-// }, (err, { data }) => {
-//     console.log(err || data)
-// })
+};
