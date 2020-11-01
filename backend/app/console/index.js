163a20518a5c6be1c5bc365031ba027ba1a0a042
JoseContreras <jocontrera16@gmail.com>
2020-11-01 01:31:51 -0300

    modifying news proccess

--- backend/app/console/index.js
+++ backend/app/console/index.js
@@ -1,18 +1,19 @@
-import {saveScraping} from '../tools/scrapeit/index.js';
-import { Scraper } from '../db/models/Scraper.js';
-import { mongoConfig } from '../db/dbSettings.js';
+import {saveScraping} from '../tool/scrape-it/index';
+import { scraper } from '../db/models/scrapers';
+import {mongoConnect} from '../db/connection';
+import config from 'config';
 
 
 console.log("SCRAPER METHOD BEGINNING");
 
 const scrapingProcess = async () => {
-    const scrapers = await Scraper.find({}).exec();
+    const scrapers = await scraper.find({}).exec();
     scrapers.forEach(async (scraper) => {
         await saveScraping(scraper.url, scraper.structure);
     });
 }
 
-mongoConfig();
+mongoConnect(config);
 scrapingProcess();
 
 console.log("SCRAPER METHOD ENDING");
